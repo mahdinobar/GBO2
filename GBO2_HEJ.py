68ef285c308b0e6e3e363d7f67e39a3352eed066
mahdinobar <nobar.mahdi@gmail.com>
2025-04-17 13:56:41 +0200

    test_31_1

--- GBO2_HEJ.py
+++ GBO2_HEJ.py
@@ -70,7 +70,7 @@ def generate_initial_data(n_IS1, n_IS2):
     # # TODO: uncomment for deterministic initial dataset of just IS1
     # train_f = fidelities[torch.randint(2,3, (n, 1))]
     # # TODO: uncomment for n_IS1 and n_IS2 data
-    train_f = fidelities[(torch.cat([torch.ones((n_IS1, 1)) * 1, torch.ones((n_IS2, 1)) * 0])).to(torch.int)]
+    train_f = fidelities[(torch.cat([torch.ones((n_IS1, 1)) * 2, torch.ones((n_IS2, 1)) * 1])).to(torch.int)]
     # train_f = fidelities[torch.randint(2, (n, 1))]
     train_x_full = torch.cat((train_x, train_f), dim=1)
     train_obj = problem(train_x_full).unsqueeze(-1)  # add output dimension
@@ -128,7 +128,7 @@ class ExpectedImprovementWithCost(AcquisitionFunction):
     factor that reduces or increases the emphasis of the cost model c(x).
     """
 
-    def __init__(self, model, cost_model, best_f_s1, best_f_s2, alpha=1):
+    def __init__(self, model, cost_model, best_f_s1, best_f_s2,best_f_s3, alpha=1):
         super().__init__(model=model)
         self.model = model
         self.cost_model = cost_model
@@ -136,34 +136,35 @@ class ExpectedImprovementWithCost(AcquisitionFunction):
         # self.ei_s2 = qExpectedImprovement(model=model, best_f=best_f_s2)
         self.best_f_s1 = best_f_s1
         self.best_f_s2 = best_f_s2
+        self.best_f_s3 = best_f_s3
         self.alpha = alpha
         self.X_pending = None
 
     @t_batch_mode_transform()
     def forward(self, X):
         # for i in range(X[:, :, -1].__len__()):
-        fidelities = X[:, :, -1].squeeze(-1)
-        best_f_s = torch.where(fidelities == 1, self.best_f_s1, self.best_f_s2)
-        # # Chain torch.where for more than 2 IS
-        # A = torch.where(fidelities == s1, best_f_s1,
-        #                 torch.where(fidelities == s2, best_f_s2, best_f_s3))
+        fidelities_ = X[:, :, -1].squeeze(-1)
+        # best_f_s = torch.where(fidelities_ == 1, self.best_f_s1, self.best_f_s2)
+        best_f_s = torch.where(fidelities_ == 1, self.best_f_s1,
+                         torch.where(fidelities_ == 0.1, self.best_f_s2, self.best_f_s3))
         self.ei = qExpectedImprovement(model=model, best_f=best_f_s)
         return self.ei(X) / torch.pow(self.cost_model(X)[:, 0], self.alpha).squeeze()
 
 
-def get_cost_aware_ei(model, cost_model, best_f_s1, best_f_s2, alpha):
+def get_cost_aware_ei(model, cost_model, best_f_s1, best_f_s2, best_f_s3, alpha):
     eipu = ExpectedImprovementWithCost(
         model=model,
         cost_model=cost_model,
         best_f_s1=best_f_s1,
         best_f_s2=best_f_s2,
+        best_f_s3=best_f_s3,
         alpha=alpha,
     )
     return eipu
 
 
 def optimize_caEI_and_get_observation(caEI):
-    candidates, _ = optimize_acqf_mixed(acq_function=caEI, bounds=bounds, fixed_features_list=[{2: 0.1}, {2: 1.0}],
+    candidates, _ = optimize_acqf_mixed(acq_function=caEI, bounds=bounds, fixed_features_list=[{2: 0.05}, {2: 0.1}, {2: 1.0}],
                                         q=BATCH_SIZE,
                                         num_restarts=NUM_RESTARTS, raw_samples=RAW_SAMPLES,
                                         options={"batch_limit": 4, "maxiter": 50}, )
@@ -184,7 +185,7 @@ def optimize_mfkg_and_get_observation(mfkg_acqf):
     candidates, _ = optimize_acqf_mixed(
         acq_function=mfkg_acqf,
         bounds=bounds,
-        fixed_features_list=[{2: 0.1}, {2: 1.0}],
+        fixed_features_list=[{2: 0.05},{2: 0.1}, {2: 1.0}],
         q=BATCH_SIZE,
         num_restarts=NUM_RESTARTS,
         raw_samples=RAW_SAMPLES,
@@ -204,7 +205,7 @@ def optimize_mfkg_and_get_observation(mfkg_acqf):
 def plot_GP(model, iter, path, train_x):
     # Step 3: Define fidelity levels and create a grid for plotting
     # uncomment for my idea
-    fidelities = [1.0, 0.1]  # Three fidelity levels
+    fidelities = [1.0, 0.1, 0.05]  # Three fidelity levels
     # fidelities = [1.0, 0.5]
     x1 = torch.linspace(0, 1, 50)
     x2 = torch.linspace(0, 1, 50)
@@ -262,7 +263,7 @@ def plot_GP(model, iter, path, train_x):
 def plot_EIonly_GP(model, iter, path, train_x):
     # Step 3: Define fidelity levels and create a grid for plotting
     # uncomment for my idea
-    fidelities = [1.0, 0.1]  # Three fidelity levels
+    fidelities = [1.0, 0.1, 0.05]  # Three fidelity levels
     # fidelities = [1.0, 0.5]
     x1 = torch.linspace(0, 1, 50)
     x2 = torch.linspace(0, 1, 50)
@@ -317,7 +318,7 @@ def plot_EIonly_GP(model, iter, path, train_x):
 def plot_UCBonly_GP(model, iter, path, train_x):
     # Step 3: Define fidelity levels and create a grid for plotting
     # uncomment for my idea
-    fidelities = [1.0, 0.1]  # Three fidelity levels
+    fidelities = [1.0, 0.1, 0.05]  # Three fidelity levels
     # fidelities = [1.0, 0.5]
     x1 = torch.linspace(0, 1, 50)
     x2 = torch.linspace(0, 1, 50)
@@ -420,7 +421,7 @@ def get_ei(model, best_f):
         values=[1],
     )
 
-def get_ucb(model, beta: float = 0.1):
+def get_ucb(model, beta: float = 0.2):
     return FixedFeatureAcquisitionFunction(
         acq_function=qUpperConfidenceBound(model=model, beta=beta),
         d=3,                      # total dimension (2 params + 1 fidelity)
@@ -476,7 +477,7 @@ for exper in range(N_exper):
     print("**********Experiment {}**********".format(exper))
     # /cluster/home/mnobar/code/GBO2
     # /home/nobar/codes/GBO2
-    path = "/cluster/home/mnobar/code/GBO2/logs/test_33_b_1/Exper_{}".format(str(exper))
+    path = "/cluster/home/mnobar/code/GBO2/logs/test_31_1/Exper_{}".format(str(exper))
     # Check if the directory exists, if not, create it
     if not os.path.exists(path):
         os.makedirs(path)
@@ -486,8 +487,7 @@ for exper in range(N_exper):
     problem.y_GP_train = None
 
     # uncomment for my idea
-    fidelities = torch.tensor([0.1, 1.0], **tkwargs)
-    # fidelities = torch.tensor([0.5, 1.0], **tkwargs)
+    fidelities = torch.tensor([0.05, 0.1, 1.0], **tkwargs)
 
     # Define the bounds
     original_bounds = torch.tensor([[30, 2, 0.0], [200, 10, 1.0]], **tkwargs)
@@ -526,7 +526,8 @@ for exper in range(N_exper):
 
         caEI = get_cost_aware_ei(model, cost_model,
                                  best_f_s1=train_obj[np.argwhere(train_x[:, 2] == 1)].squeeze().max(),
-                                 best_f_s2=train_obj[np.argwhere(train_x[:, 2] == 0.1)].squeeze().max(), alpha=1)
+                                 best_f_s2=train_obj[np.argwhere(train_x[:, 2] == 0.1)].squeeze().max(),
+                                 best_f_s3=train_obj[np.argwhere(train_x[:, 2] == 0.05)].squeeze().max(), alpha=1)
         new_x, new_obj, cost = optimize_caEI_and_get_observation(caEI)
 
         # fixed_features_list = [
