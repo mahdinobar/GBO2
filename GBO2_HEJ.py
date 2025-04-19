9414693d5b2b2896a365935d82bc4e04fe93f378
mahdinobar <nobar.mahdi@gmail.com>
2025-04-19 15:01:01 +0200

    test_34_3

--- GBO2_HEJ.py
+++ GBO2_HEJ.py
@@ -475,7 +475,7 @@ for exper in range(N_exper):
     print("**********Experiment {}**********".format(exper))
     # /cluster/home/mnobar/code/GBO2
     # /home/nobar/codes/GBO2
-    path = "/cluster/home/mnobar/code/GBO2/logs/test_34_1/Exper_{}".format(str(exper))
+    path = "/cluster/home/mnobar/code/GBO2/logs/test_34_3/Exper_{}".format(str(exper))
     # Check if the directory exists, if not, create it
     if not os.path.exists(path):
         os.makedirs(path)
