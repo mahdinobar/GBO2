3ff55e7209c62f84f220637f3856e8403385eb68
mahdinobar <nobar.mahdi@gmail.com>
2025-04-20 10:42:31 +0200

    test_34_2 plot added

--- plots.py
+++ plots.py
@@ -631,9 +631,9 @@ def plots_MonteCarlo_objective(path,    N_init_IS1,N_init_IS2,    sampling_cost_
     plt.savefig(path+"/J_min_obs_IS1_Mean_Unbiased_cost_sampling.png")
     plt.show()
 
-    mean_values_baseline=np.load("/home/nobar/codes/GBO2/logs/test_6_11_baseline/mean_values.npy")
-    margin_of_error_baseline=np.load("/home/nobar/codes/GBO2/logs/test_6_11_baseline/margin_of_error.npy")
-    x_baseline=np.load("/home/nobar/codes/GBO2/logs/test_6_11_baseline/x.npy")
+    # mean_values_baseline=np.load("/home/nobar/codes/GBO2/logs/test_6_11_baseline/mean_values.npy")
+    # margin_of_error_baseline=np.load("/home/nobar/codes/GBO2/logs/test_6_11_baseline/margin_of_error.npy")
+    # x_baseline=np.load("/home/nobar/codes/GBO2/logs/test_6_11_baseline/x.npy")
 
     # Compute statistics
     mean_values = np.mean(j_min_observed_IS1, axis=1)  # Mean of each element (over 20 vectors)
@@ -702,6 +702,236 @@ def plots_MonteCarlo_objective(path,    N_init_IS1,N_init_IS2,    sampling_cost_
     # np.save("/home/nobar/codes/GBO2/logs/test_6_11_baseline/margin_of_error.npy",margin_of_error)
     # np.save("/home/nobar/codes/GBO2/logs/test_6_11_baseline/x.npy",x)
     print("")
+
+def plots_MonteCarlo_objectiveEI_34tests(path, path2,   N_init_IS1,N_init_IS2,    sampling_cost_bias,N_exper,N_iter, s2, s3, BATCH_SIZE):
+    costs_all_list = []
+    costs_all_list_EIonly = []
+    train_x_list = []
+    train_obj_list = []
+    train_x_list_IS1 = []
+    train_obj_list_IS1 = []
+    train_x_list_EIonly = []
+    train_obj_list_EIonly = []
+    idx_IS1_all=[]
+    idx_IS1_all_init=[]
+    idx_IS1_all_rest=[]
+    idx_IS2_all=[]
+    idx_IS2_all_init=[]
+    idx_IS2_all_rest=[]
+    idx_IS3_all=[]
+    idx_IS3_all_init=[]
+    idx_IS3_all_rest=[]
+    idx_ISDTs_all=[]
+    idx_ISDTs_all_init=[]
+    idx_ISDTs_all_rest=[]
+    # train_x_IS1_init_list=[]
+    # train_obj_IS1_init_list=[]
+    train_obj_EIonly_corrected_all=[]
+    min_obj_init_all=[]
+    train_obj_list_rest_modified=[]
+    costs_init=[]
+    costs_init_IS1=[]
+    costs_init_EIonly=[]
+    for exper in range(N_exper):
+        # if exper==2:
+        #     exper=0
+        exp_path = os.path.join(path, f"Exper_{exper}")
+        exp_path2 = os.path.join(path2, f"Exper_{exper}")
+        # Load files
+        train_x = np.load(os.path.join(exp_path, "train_x.npy"))
+        train_obj = np.load(os.path.join(exp_path, "train_obj.npy"))
+
+        idx_IS1 = np.argwhere(train_x[:, 2] == 1).squeeze()
+        idx_IS1_init=idx_IS1[np.argwhere(idx_IS1<N_init_IS1*2+N_init_IS2)[:,0]]
+        idx_IS1_rest=idx_IS1[np.argwhere(idx_IS1>N_init_IS1*2+N_init_IS2-1)[:,0]]
+        idx_ISDTs = np.argwhere(~(train_x[:, 2] == 1)).squeeze()
+        idx_ISDTs_init=idx_ISDTs[np.argwhere(idx_ISDTs<N_init_IS1*2+N_init_IS2)[:,0]]
+        idx_ISDTs_rest=idx_ISDTs[np.argwhere(idx_ISDTs>N_init_IS1*2+N_init_IS2-1)[:,0]]
+        train_x_IS1_init=train_x[idx_IS1_init, :]
+        train_obj_IS1_init=train_obj[idx_IS1_init]
+
+        idx_IS2 = np.argwhere(train_x[:, 2] == s2).squeeze()
+        idx_IS2 = idx_IS2.reshape(idx_IS2.size, )
+        idx_IS2_init=idx_IS2[np.argwhere(idx_IS2<N_init_IS1+N_init_IS2)[:,0]]
+        idx_IS2_rest=idx_IS2[np.argwhere(idx_IS2>N_init_IS1+N_init_IS2-1)[:,0]]
+        idx_IS3 = np.argwhere(train_x[:, 2] == s3).squeeze()
+        idx_IS3=idx_IS3.reshape(idx_IS3.size, )
+        idx_IS3_init=idx_IS3[np.argwhere(idx_IS3<N_init_IS1+N_init_IS2)[:,0]]
+        idx_IS3_rest=idx_IS3[np.argwhere(idx_IS3>N_init_IS1+N_init_IS2-1)[:,0]]
+
+        # np.save(os.path.join(exp_path, "train_x_IS1_init.npy"),train_x_IS1_init)
+        # np.save(os.path.join(exp_path, "train_obj_IS1_init.npy"),train_obj_IS1_init)
+
+        min_obj_init_all.append(np.min(-train_obj_IS1_init))
+
+        train_x_IS1 = train_x[idx_IS1]
+        train_obj_IS1 = train_obj[idx_IS1]
+        train_x_EIonly = np.load(os.path.join(exp_path2, "train_x_EIonly.npy"))
+        train_x_EIonly_corrected = np.load(os.path.join(exp_path2, "train_x_EIonly.npy"))
+        train_obj_EIonly_corrected = np.load(os.path.join(exp_path2, "train_obj_EIonly.npy"))
+
+        train_obj_EIonly = np.load(os.path.join(exp_path2, "train_obj_EIonly.npy"))
+
+        # train_x_EIonly=np.delete(train_x_EIonly, np.s_[N_init_IS1:N_init_IS1 + N_init_IS2], axis=0)
+        # train_obj_EIonly=np.delete(train_obj_EIonly, np.s_[N_init_IS1:N_init_IS1 + N_init_IS2], axis=0)
+
+        train_obj_EIonly[N_init_IS1:N_init_IS1 + N_init_IS2]
+        costs_all=np.load(os.path.join(exp_path, "costs_all.npy"))
+        costs_all_EIonly=np.load(os.path.join(exp_path2, "costs_all_EIonly.npy"))
+        costs_all_EIonly_corrected=np.load(os.path.join(exp_path2, "costs_all_EIonly.npy"))
+        # Append to lists
+        costs_all_list.append(costs_all)
+        costs_init.append(np.sum(train_x[:N_init_IS1+N_init_IS2,2])+sampling_cost_bias*(N_init_IS1+N_init_IS2))
+        costs_init_IS1.append(np.sum(train_x[:N_init_IS1,2])+sampling_cost_bias*(N_init_IS1))
+        costs_init_EIonly.append(np.sum(train_x_EIonly[:N_init_IS1,2])+sampling_cost_bias*(N_init_IS1))
+
+        idx_IS1_all.append(idx_IS1)
+        idx_IS1_all_init.append(idx_IS1_init)
+        idx_IS1_all_rest.append(idx_IS1_rest)
+
+        idx_IS2_all.append(idx_IS2)
+        idx_IS2_all_init.append(idx_IS2_init)
+        idx_IS2_all_rest.append(idx_IS2_rest)
+        idx_IS3_all.append(idx_IS3)
+        idx_IS3_all_init.append(idx_IS3_init)
+        idx_IS3_all_rest.append(idx_IS3_rest)
+
+        idx_ISDTs_all.append(idx_ISDTs)
+        idx_ISDTs_all_init.append(idx_ISDTs_init)
+        idx_ISDTs_all_rest.append(idx_ISDTs_rest)
+
+        costs_all_list_EIonly.append(costs_all_EIonly)
+        train_x_list.append(train_x)
+        train_obj_list.append(train_obj)
+
+        A=train_obj
+        A[idx_ISDTs_rest]=-np.inf
+        train_obj_list_rest_modified.append(-A[N_init_IS1*2+N_init_IS2:])
+
+        train_x_list_IS1.append(train_x_IS1)
+        train_obj_list_IS1.append(train_obj_IS1)
+        # train_x_IS1_init_list.append(train_x_IS1_init)
+        # train_obj_IS1_init_list.append(train_obj_IS1_init)
+        train_x_list_EIonly.append(train_x_EIonly)
+        train_obj_EIonly_corrected_all.append(train_obj_EIonly_corrected)
+        train_obj_list_EIonly.append(train_obj_EIonly)
+
+    # Step 1: Determine the maximum number of rows (n)
+    max_len = max(arr.shape[0] for arr in train_obj_list_rest_modified)
+    # Step 2: Initialize output array with np.inf
+    DD = np.full((N_exper, max_len), np.inf)
+    costsALL = np.full((max_len,N_exper), np.inf)
+    costsIS1only = np.full((max_len,N_exper), np.inf)
+    # Step 3: Fill in the values
+    for i, arr in enumerate(train_obj_list_rest_modified):
+        DD[i, :arr.shape[0]] = arr.flatten()
+        DDi = np.where(np.isinf(arr), s2, 1.0)
+        x_col=np.cumsum(DDi, axis=0)
+        num_to_add = max_len - x_col.shape[0]
+        last_row = x_col[-1:, :]  # keep it 2D shape (1,1)
+        padding = np.repeat(last_row, num_to_add, axis=0)
+        x_padded = np.vstack([x_col, padding])
+        costsALL[:,i]=x_padded.flatten()
+        DDi = np.where(np.isinf(arr), 0, 1.0)
+        x_col=np.cumsum(DDi, axis=0)
+        num_to_add = max_len - x_col.shape[0]
+        last_row = x_col[-1:, :]  # keep it 2D shape (1,1)
+        padding = np.repeat(last_row, num_to_add, axis=0)
+        x_padded = np.vstack([x_col, padding])
+        costsIS1only[:,i]=x_padded.flatten()
+
+    row_to_add = np.full((1, N_exper), N_init_IS1)
+    costsIS1only = np.vstack([row_to_add, costsIS1only+2])
+
+    row_to_addALL = np.full((1, N_exper), N_init_IS1+(N_init_IS2+2)*s2)
+    costsALL = np.vstack([row_to_addALL, costsALL+2])
+
+    D=np.vstack((np.asarray(min_obj_init_all), DD.T))
+    j_min_observed_IS1=np.minimum.accumulate(D, axis=0)
+    mean_values = np.mean(j_min_observed_IS1, axis=1)  # Mean of each element (over 20 vectors)
+    std_values = np.std(j_min_observed_IS1, axis=1, ddof=1)
+    # Compute 95% confidence interval
+    n = j_min_observed_IS1.shape[1]
+    t_value = t.ppf(0.975, df=n - 1)  # t-score for 95% CI
+    margin_of_error = t_value * (std_values / np.sqrt(n))  # CI width
+    mean_values_costsIS1only = np.mean(costsIS1only, axis=1)  # Mean of each element (over 20 vectors)
+
+
+    costs_IS2_all=[]
+    costs_IS3_all=[]
+    for i in range(N_exper):
+        costs_IS2_all_=[]
+        costs_IS3_all_=[]
+        for j in range(N_iter):
+            costs_IS2_all_.append(np.sum((idx_IS2_all_rest[i] < BATCH_SIZE * (j+1)) * (idx_IS2_all_rest[i] > BATCH_SIZE * j)) * s2)
+            costs_IS3_all_.append(np.sum((idx_IS3_all_rest[i] < BATCH_SIZE * (j+1)) * (idx_IS3_all_rest[i] > BATCH_SIZE * j)) * s3)
+        costs_IS2_all.append(costs_IS2_all_)
+        costs_IS3_all.append(costs_IS3_all_)
+    old_indices = np.linspace(0, 1, num=1+N_iter)  # Original index positions
+    new_indices = np.linspace(0, 1, num=1+BATCH_SIZE*N_iter)  # New index positions
+    # # Compute statistics
+    # mean_values = np.mean(j_min_observed_IS1, axis=1)  # Mean of each element (over 20 vectors)
+    # std_values = np.std(j_min_observed_IS1, axis=1, ddof=1)  # Sample standard deviation
+    # # Compute 95% confidence interval
+    # n = j_min_observed_IS1.shape[1]  # Number of samples (20)
+    # t_value = t.ppf(0.975, df=n - 1)  # t-score for 95% CI
+    # margin_of_error = t_value * (std_values / np.sqrt(n))  # CI width
+    DD=-np.stack(train_obj_list_EIonly).squeeze()[:,N_init_IS1:].T
+    D=np.vstack((np.asarray(min_obj_init_all), DD))
+    j_EIonly_min_observed_IS1=np.minimum.accumulate(D, axis=0)
+    # J_EIonly_mean=np.mean(j_EIonly_min_observed_IS1,axis=1)
+    # J_EIonly_std=np.std(j_EIonly_min_observed_IS1,axis=1)
+    mean_values_EIonly = np.mean(j_EIonly_min_observed_IS1, axis=1)  # Mean of each element (over 20 vectors)
+    std_values_EIonly = np.std(j_EIonly_min_observed_IS1, axis=1, ddof=1)  # Sample standard deviation
+    # Compute 95% confidence interval
+    n = j_EIonly_min_observed_IS1.shape[1]  # Number of samples (20)
+    t_value = t.ppf(0.975, df=n - 1)  # t-score for 95% CI
+    margin_of_error_EIonly = t_value * (std_values_EIonly / np.sqrt(n))  # CI width
+    # costs_IS1=np.hstack((np.asarray(costs_init_IS1).reshape(N_exper,1)-sampling_cost_bias*(N_init_IS1),np.stack(costs_all_list)[:,:N_iter]-np.stack(costs_IS2_all)-np.stack(costs_IS3_all)-sampling_cost_bias*BATCH_SIZE))
+    # C_IS1=np.cumsum(costs_IS1, axis=1)
+    # C_mean_IS1=np.mean(C_IS1,axis=0)
+    # x_IS1_ = C_mean_IS1 #np.arange(0,BATCH_SIZE*N_iter+1,BATCH_SIZE)
+    # x_IS1 = np.interp(new_indices, old_indices, x_IS1_)
+    # costs=np.hstack((np.asarray(costs_init).reshape(N_exper,1)-sampling_cost_bias*(N_init_IS1+N_init_IS2),np.stack(costs_all_list)-sampling_cost_bias*BATCH_SIZE))
+    # C=np.cumsum(costs, axis=1)
+    # C_mean=np.mean(C,axis=0)
+    # C_std=np.std(C,axis=0)
+    costs_EIonly=np.hstack((np.asarray(costs_init_EIonly).reshape(N_exper,1)-sampling_cost_bias*(N_init_IS1),np.ones((N_exper,N_iter))*(1)*BATCH_SIZE))
+    C_EIonly=np.cumsum(costs_EIonly, axis=1)
+    C_EIonly_mean=np.mean(C_EIonly,axis=0)
+    C_EIonly_std=np.std(C_EIonly,axis=0)
+    # x_ = C_mean[:N_iter+1] #np.arange(0,BATCH_SIZE*N_iter+1,BATCH_SIZE)
+    # linear interpolation
+    # x = np.interp(new_indices, old_indices, x_)
+    x_EI_only_ = C_EIonly_mean #np.arange(0,BATCH_SIZE*N_iter+1,BATCH_SIZE)
+    # linear interpolation
+    x_EI_only = np.interp(new_indices, old_indices, x_EI_only_)
+
+
+    # Plot
+    plt.figure(figsize=(10, 5))
+    plt.plot(mean_values_costsIS1only, mean_values, marker="o", linewidth=3, label="Mean GMFBO", color='r')
+    plt.fill_between(mean_values_costsIS1only, mean_values - margin_of_error, mean_values + margin_of_error,
+                     color='r', alpha=0.3, label="95% CI GMFBO")
+    # plt.plot(x_baseline, mean_values_baseline, marker="o", linewidth=3, label="Mean MFBO", color='k')
+    # plt.fill_between(x_baseline, mean_values_baseline - margin_of_error_baseline, mean_values_baseline + margin_of_error_baseline,
+    #                  color='k', alpha=0.3, label="95% CI MFBO")
+    plt.plot(x_EI_only, mean_values_EIonly, marker="o", linewidth=3, label="Mean BO-EI", color='b')
+    plt.fill_between(x_EI_only, mean_values_EIonly - margin_of_error_EIonly, mean_values_EIonly + margin_of_error_EIonly,
+                     color='b', alpha=0.3, label="95% CI BO-EI")
+    plt.xlabel('Mean IS1 only Sampling Cost')
+    plt.ylabel('$J^{*}$')
+    plt.legend()
+    plt.grid(True)
+    # plt.ylim(0.9, 1.4)  # Focus range
+    plt.title("Mean with 95% Confidence Interval")
+    plt.savefig(path+"/Obj_min_obs_IS1_Mean_IS1onlySamplingCost_95Conf.png")
+    plt.show()
+
+
+    print("")
+
+
 
 def plots_MonteCarlo_objectiveEI(path, path2,   N_init_IS1,N_init_IS2,    sampling_cost_bias,N_exper,N_iter, s2, s3, BATCH_SIZE):
     costs_all_list = []
@@ -1621,7 +1851,7 @@ def plot_real():
 
 if __name__ == "__main__":
 
-    plot_gt()
+    # plot_gt()
     # plot_real()
 
     # # check objective scales
@@ -1640,9 +1870,9 @@ if __name__ == "__main__":
 
     # plot_cost_coef()
 
-    path = "/home/nobar/codes/GBO2/logs/test_33_1/"
+    path = "/home/nobar/codes/GBO2/logs/test_34_2/"
     # path2 = "/home/nobar/codes/GBO2/logs/test_31_b_UCB_1/"
-    path2 = "/home/nobar/codes/GBO2/logs/test_33_1/"
+    path2 = "/home/nobar/codes/GBO2/logs/test_33_b_1/"
     N_init_IS1=2
     N_init_IS2=10
     sampling_cost_bias=5
@@ -1672,18 +1902,19 @@ if __name__ == "__main__":
     #         plot_EIonly_GP(j, path+"Exper_"+str(i)+"/", train_x_i,train_obj_i)
 
 
-    # validate identical initial dataset
-    for i in range(N_exper):
-        x=np.load(path+"Exper_{}/train_x_init.npy".format(str(i)))
-        y=np.load(path+"Exper_{}/train_obj_init.npy".format(str(i)))
-        x2=np.load(path2+"Exper_{}/train_x_init.npy".format(str(i)))
-        y2=np.load(path+"Exper_{}/train_obj_init.npy".format(str(i)))
-        diffx=x[:2,:]-x2[:2,:]
-        diffy=y[:2,:]-y2[:2,:]
-        if np.sum(diffx)+np.sum(diffy)!=0:
-            print("Experiment i=",i,ValueError("ERROR: initial dataset not identical across trials!"))
+    # # validate identical initial dataset
+    # for i in range(N_exper):
+    #     x=np.load(path+"Exper_{}/train_x_init.npy".format(str(i)))
+    #     y=np.load(path+"Exper_{}/train_obj_init.npy".format(str(i)))
+    #     x2=np.load(path2+"Exper_{}/train_x_init.npy".format(str(i)))
+    #     y2=np.load(path+"Exper_{}/train_obj_init.npy".format(str(i)))
+    #     diffx=x[:2,:]-x2[:2,:]
+    #     diffy=y[:2,:]-y2[:2,:]
+    #     if np.sum(diffx)+np.sum(diffy)!=0:
+    #         print("Experiment i=",i,ValueError("ERROR: initial dataset not identical across trials!"))
 
-    plots_MonteCarlo_objective(path,N_init_IS1,N_init_IS2,sampling_cost_bias,N_exper,N_iter,s2,s3, BATCH_SIZE)
+    # plots_MonteCarlo_objective(path,N_init_IS1,N_init_IS2,sampling_cost_bias,N_exper,N_iter,s2,s3, BATCH_SIZE)
     # plots_MonteCarlo_objectiveEI(path,path2,N_init_IS1,N_init_IS2,sampling_cost_bias,N_exper,N_iter,s2,s3, BATCH_SIZE)
+    plots_MonteCarlo_objectiveEI_34tests(path,path2,N_init_IS1,N_init_IS2,sampling_cost_bias,N_exper,N_iter,s2,s3, BATCH_SIZE)
     # plots_MonteCarlo_objectiveUCB(path,path2,N_init_IS1,N_init_IS2,sampling_cost_bias,N_exper,N_iter,s2,s3, BATCH_SIZE)
 
